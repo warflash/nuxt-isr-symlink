f93237f198ef93240a6b0ab87239fe03a8c674b1
warflash <nwiesinger67@gmail.com>
2023-08-05 02:14:33 +0200

    1 file modified

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,13 +3,7 @@ export default defineNuxtConfig({
   routeRules: {
     '/en/NA/*/**': { isr: 7200 },
     '/en/EU/*/**': { isr: 7200 },
-    '/en/MENA/*/**': { isr: 7200 },
-    '/en/RU/*/**': { isr: 7200 },
-    '/en/SEA/*/**': { isr: 7200 },
     '/de/NA/*/**': { isr: 7200 },
     '/de/EU/*/**': { isr: 7200 },
-    '/de/MENA/*/**': { isr: 7200 },
-    '/de/RU/*/**': { isr: 7200 },
-    '/de/SEA/*/**': { isr: 7200 },
   },
 });
